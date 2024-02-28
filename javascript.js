44177192ec2aadf3727cf00b1e24702b8ec51ddd
Sauravroy34 <sauravnotfound29@gmail.com>
2024-02-28 18:42:43 +0530

    now multiple meanings can be shown

--- javascript.js
+++ javascript.js
@@ -1,19 +1,52 @@
-
-// Ensure you have an element with ID "city" in your HTML
-
+var x = 0;
 const button = document.getElementById("search");
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
+
 button.addEventListener("click", () => {
-    const word = document.getElementById("city").value || "default_word"; // Handle empty input
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data[0].word);
-            document.getElementById("name").innerHTML=data[0].word
-            means = (data[0].meanings[0].definitions[0].definition)
-            example= (data[0].meanings[0].definitions[0].example)
-            console.log(means)
-            console.log(example)
-            document.getElementById("tem").innerHTML=means
-            document.getElementById("example"),innerHTML= example
-        });
+  const word = document.getElementById("city").value || "default_word"; 
+
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const definitions = {}; 
+
+      
+      for (const meaning of data[0].meanings) {
+        for (const definition of meaning.definitions) {
+          definitions[definition.definition] = true;
+        }
+      }
+
+      console.log(definitions); 
+
+      
+      document.getElementById("name").innerHTML = data[0].word;
+      document.getElementById("tem").innerHTML = ""; 
+      let nextDefinition = Object.keys(definitions)[x]; 
+      document.getElementById("tem").innerHTML = nextDefinition; 
+
+      next.addEventListener("click", () => {
+        x++;
+        if (x >= Object.keys(definitions).length) {
+          x = 0; 
+        }
+        nextDefinition = Object.keys(definitions)[x];
+        document.getElementById("tem").innerHTML = nextDefinition;
+      });
+
+      previous.addEventListener("click", () => {
+        x--;
+        if (x < 0) {
+          x = Object.keys(definitions).length - 1;
+        }
+        nextDefinition = Object.keys(definitions)[x];
+        document.getElementById("tem").innerHTML = nextDefinition;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+     
+    });
 });
+
